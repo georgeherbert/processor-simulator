1f92641131b60f5b8f57b349c7f22bbffde78ddb
George Herbert <george.herbert@me.com>
2023-02-11 15:23:33 +0000

    Started writing assembler

--- simple-simulator/processor.c
+++ simple-simulator/processor.c
@@ -48,5 +48,21 @@ rv32i_instruction *instructions;
 
 int main(int argc, char *argv[])
 {
+    FILE *fp;
+    rv32i_instruction s;
+
+    fp = fopen("test.o", "rb");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    while (fread(&s, sizeof(rv32i_instruction), 1, fp) == 1) {
+        printf("opcode: %d\n", s.u_type.opcode);
+        printf("rd: %d\n", s.u_type.rd);
+        printf("imm: %d\n", s.u_type.imm);
+    }
+
+    fclose(fp);
     return 0;
 }
