46ea8b5f6dfa4c5355bdc3bb17fe77fa1cb476f3
George Herbert <george.herbert@me.com>
2023-02-13 11:09:27 +0000

    Assembler now complete and works

--- assembler.c
+++ assembler.c
@@ -184,7 +184,8 @@ void word_lists_to_machine_code(node *head, rv32i_instruction *machine_code)
 
     while (current != NULL)
     {
-        char *operation = current->words[0];
+        char **fields = current->words;
+        char *operation = fields[0];
         uint8_t num_fields = current->num_words;
 
         // Integer Register-Immediate
@@ -192,92 +193,313 @@ void word_lists_to_machine_code(node *head, rv32i_instruction *machine_code)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = ADDI_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_intn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
         }
         else if (strcmp(operation, "slti") == 0 && num_fields == 4)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = SLTI_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_intn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
         }
         else if (strcmp(operation, "sltiu") == 0 && num_fields == 4)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = SLTIU_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_uintn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_uintn_t(fields[3], 12);
         }
         else if (strcmp(operation, "andi") == 0 && num_fields == 4)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = ANDI_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_intn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
         }
         else if (strcmp(operation, "ori") == 0 && num_fields == 4)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = ORI_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_intn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
         }
         else if (strcmp(operation, "xori") == 0 && num_fields == 4)
         {
             machine_code[i].i_type.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type.funct3 = XORI_FUNCT3;
-            machine_code[i].i_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type.imm = str_imm_to_intn_t(current->words[3], 12);
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
         }
         else if (strcmp(operation, "slli") == 0 && num_fields == 4)
         {
             machine_code[i].i_type_shift.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type_shift.funct3 = SLLI_FUNCT3;
-            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(current->words[3], 5);
+            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(fields[3], 5);
             machine_code[i].i_type_shift.shift_type = SLLI_SHIFT_TYPE;
         }
         else if (strcmp(operation, "srli") == 0 && num_fields == 4)
         {
             machine_code[i].i_type_shift.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type_shift.funct3 = SRLI_FUNCT3;
-            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(current->words[3], 5);
+            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(fields[3], 5);
             machine_code[i].i_type_shift.shift_type = SRLI_SHIFT_TYPE;
         }
         else if (strcmp(operation, "srai") == 0 && num_fields == 4)
         {
             machine_code[i].i_type_shift.opcode = OP_IMM_OPCODE;
             machine_code[i].i_type_shift.funct3 = SRAI_FUNCT3;
-            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(current->words[2]);
-            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(current->words[3], 5);
+            machine_code[i].i_type_shift.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type_shift.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type_shift.shamt = str_imm_to_uintn_t(fields[3], 5);
             machine_code[i].i_type_shift.shift_type = SRAI_SHIFT_TYPE;
         }
         else if (strcmp(operation, "lui") == 0 && num_fields == 3)
         {
             machine_code[i].u_type.opcode = LUI_OPCODE;
-            machine_code[i].u_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].u_type.imm = str_imm_to_uintn_t(current->words[2], 20);
+            machine_code[i].u_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].u_type.imm = str_imm_to_uintn_t(fields[2], 20);
         }
         else if (strcmp(operation, "auipc") == 0 && num_fields == 3)
         {
             machine_code[i].u_type.opcode = AUIPC_OPCODE;
-            machine_code[i].u_type.rd = str_register_to_uint5_t(current->words[1]);
-            machine_code[i].u_type.imm = str_imm_to_uintn_t(current->words[2], 20);
+            machine_code[i].u_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].u_type.imm = str_imm_to_uintn_t(fields[2], 20);
         }
 
         // Integer Register-Register
+        else if (strcmp(operation, "add") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = ADD_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = ADD_FUNCT7;
+        }
+        else if (strcmp(operation, "slt") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SLT_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SLT_FUNCT7;
+        }
+        else if (strcmp(operation, "sltu") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SLTU_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SLTU_FUNCT7;
+        }
+        else if (strcmp(operation, "and") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = AND_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = AND_FUNCT7;
+        }
+        else if (strcmp(operation, "or") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = OR_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = OR_FUNCT7;
+        }
+        else if (strcmp(operation, "xor") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = XOR_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = XOR_FUNCT7;
+        }
+        else if (strcmp(operation, "sll") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SLL_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SLL_FUNCT7;
+        }
+        else if (strcmp(operation, "srl") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SRL_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SRL_FUNCT7;
+        }
+        else if (strcmp(operation, "sub") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SUB_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SUB_FUNCT7;
+        }
+        else if (strcmp(operation, "sra") == 0 && num_fields == 4)
+        {
+            machine_code[i].r_type.opcode = OP_OPCODE;
+            machine_code[i].r_type.funct3 = SRA_FUNCT3;
+            machine_code[i].r_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].r_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].r_type.rs2 = str_register_to_uint5_t(fields[3]);
+            machine_code[i].r_type.funct7 = SRA_FUNCT7;
+        }
 
+        // Unconditional jumps
+        else if (strcmp(operation, "jal") == 0 && num_fields == 3)
+        {
+            machine_code[i].u_type.opcode = JAL_OPCODE;
+            machine_code[i].u_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].u_type.imm = str_imm_to_intn_t(fields[2], 20);
+        }
+        else if (strcmp(operation, "jalr") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = JALR_OPCODE;
+            machine_code[i].i_type.funct3 = JALR_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+
+        // Conditional branches
+        else if (strcmp(operation, "beq") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BEQ_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "bne") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BNE_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "blt") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BLT_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "bltu") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BLTU_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "bge") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BGE_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "bgeu") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = BRANCH_OPCODE;
+            machine_code[i].s_type.funct3 = BGEU_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+
+        // Load and store
+        else if (strcmp(operation, "lw") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = LOAD_OPCODE;
+            machine_code[i].i_type.funct3 = LW_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "lh") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = LOAD_OPCODE;
+            machine_code[i].i_type.funct3 = LH_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "lhu") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = LOAD_OPCODE;
+            machine_code[i].i_type.funct3 = LHU_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "lb") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = LOAD_OPCODE;
+            machine_code[i].i_type.funct3 = LB_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "lbu") == 0 && num_fields == 4)
+        {
+            machine_code[i].i_type.opcode = LOAD_OPCODE;
+            machine_code[i].i_type.funct3 = LBU_FUNCT3;
+            machine_code[i].i_type.rd = str_register_to_uint5_t(fields[1]);
+            machine_code[i].i_type.rs1 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].i_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "sw") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = STORE_OPCODE;
+            machine_code[i].s_type.funct3 = SW_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "sh") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = STORE_OPCODE;
+            machine_code[i].s_type.funct3 = SH_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
+        else if (strcmp(operation, "sb") == 0 && num_fields == 4)
+        {
+            machine_code[i].s_type.opcode = STORE_OPCODE;
+            machine_code[i].s_type.funct3 = SB_FUNCT3;
+            machine_code[i].s_type.rs1 = str_register_to_uint5_t(fields[1]);
+            machine_code[i].s_type.rs2 = str_register_to_uint5_t(fields[2]);
+            machine_code[i].s_type.imm = str_imm_to_intn_t(fields[3], 12);
+        }
         else
         {
             perror("Error: Invalid operation or number of fields.\n");
---
+++ test_kernels/test.asm
@@ -0,0 +1,104 @@
+addi x0 x0 -2048
+addi x0 x0 2047
+
+slti x0 x0 -2048
+slti x0 x0 2047
+
+sltiu x0 x0 0
+sltiu x0 x0 4095
+
+andi x0 x0 -2048
+andi x0 x0 2047
+
+ori x0 x0 -2048
+ori x0 x0 2047
+
+xori x0 x0 -2048
+xori x0 x0 2047
+
+slli x0 x0 0
+slli x0 x0 31
+
+srli x0 x0 0
+srli x0 x0 31
+
+srai x0 x0 0
+srai x0 x0 31
+
+lui x0 0
+lui x0 1048575
+
+lui x0 0
+lui x0 1048575
+
+
+add x0 x0 x0
+
+slt x0 x0 x0
+
+sltu x0 x0 x0
+
+and x0 x0 x0
+
+or x0 x0 x0
+
+xor x0 x0 x0
+
+sll x0 x0 x0
+
+srl x0 x0 x0
+
+sub x0 x0 x0
+
+sra x0 x0 x0
+
+
+jal x0 -524288
+jal x0 524287
+
+jalr x0 x0 -2048
+jalr x0 x0 2047
+
+
+beq x0 x0 -2048
+beq x0 x0 2047
+
+bne x0 x0 -2048
+bne x0 x0 2047
+
+blt x0 x0 -2048
+blt x0 x0 2047
+
+bltu x0 x0 -2048
+bltu x0 x0 2047
+
+bge x0 x0 -2048
+bge x0 x0 2047
+
+bgeu x0 x0 -2048
+bgeu x0 x0 2047
+
+
+lw x0 x0 -2048
+lw x0 x0 2047
+
+lh x0 x0 -2048
+lh x0 x0 2047
+
+lhu x0 x0 -2048
+lhu x0 x0 2047
+
+lb x0 x0 -2048
+lb x0 x0 2047
+
+lbu x0 x0 -2048
+lbu x0 x0 2047
+
+sw x0 x0 -2048
+sw x0 x0 2047
+
+sh x0 x0 -2048
+sh x0 x0 2047
+
+sb x0 x0 -2048
+sb x0 x0 2047
